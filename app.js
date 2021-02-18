55725bbfd268c93f18589739f30feca7c2c30577
Lucia Cyprianova <l.cyprianova@gmail.com>
2021-02-18 13:38:52 +0100

    add procfile and update

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(
 );
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB", {
+mongoose.connect("mongodb+srv://admin-lucia:test123@cluster0.hjaev.mongodb.net/todolistDB", {
   useNewUrlParser: true
 });
 
@@ -169,6 +169,12 @@ app.get("/about", function(req, res) {
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+
+app.listen(port, function() {
+  console.log("Server started successfully");
 });
